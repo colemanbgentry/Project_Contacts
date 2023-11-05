5678bc80c8e233129eb6d89a649692c21f6461d1
Coleman B Gentry <104287033+colemanbgentry@users.noreply.github.com>
2023-11-04 21:01:54 -0700

    One more time

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
-//Hello There
+//Hello There, Once more
 mongodb.initDb((err) => {
     if (err) {
         console.log(err);
