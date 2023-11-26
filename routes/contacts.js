3de876bbedea226fa3588eaaea8eb178cd2c0d3d
Coleman B Gentry <104287033+colemanbgentry@users.noreply.github.com>
2023-11-25 19:54:58 -0800

    REST aquired and Swagger 2. Its all working

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', usersContoller.getAll);
 
 router.get('/:id', usersContoller.getSingle);
 
+router.post('/', usersContoller.createUser);
+
+router.put('/:id', usersContoller.updateUser);
+
+router.delete('/:id', usersContoller.deleteUser);
+
 module.exports = router;
